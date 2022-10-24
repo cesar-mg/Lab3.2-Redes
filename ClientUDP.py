f0ba71a92d35312b8c04e1198fb5ab95b80a82ee
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2022-10-23 23:16:43 -0500

    Log support

--- ClientUDP.py
+++ ClientUDP.py
@@ -1,6 +1,12 @@
+import datetime
+import os
 import socket
 import sys
 import threading
+import time
+
+lock = threading.lock()
+
 def recieve_file(file_name,sock):
     file = open(file_name, "w")
     # Leemos de a 1024 bytes
@@ -15,17 +21,36 @@ def recieve_file(file_name,sock):
     return
 
 def handle_client(client_id):
-    HOST, PORT = "localhost", 8888
-    data = "R"
+    HOST, PORT = "10.0.2.15", 8888
+    data = str(client_id)
     file_name = "ArchivosRecibidos/Cliente" + str(client_id) + "-Prueba-"+str(clientes)+".txt"
-        
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     sock.sendto((data + "\n").encode("utf-8"), (HOST, PORT))
+    print("Cliente " + str(client_id) + " conectado y recibiendo datos...")
+    init = time.time()
+
     recieve_file(file_name,sock)
 
-    print("Sent:     {}".format(data))
-    #print("Received: {}".format(received))
+    lock.acquire()
+    fin = time.time()
+    tiempo = fin - init 
+
+    file_stats = os.stat(file_name)
+    
+    date = datetime.datetime.now()
+    name = str(date.year) + "-" + str(date.month) + "-" + str(date.day) + "-" + str(date.hour) + "-" + str(date.minute) + "-" + str(date.second)
+    msg = "Nombre del archivo: " + file_name + " Tamanio del archivo: " + file_stats.st_size
+    msg += " Cliente: " + str(client_id)
+    msg += "\nEl tiempo calculado fue de: " + str(tiempo) +"\n"
+
+    log = open("Logs/"+name+"-log.txt","w")
+    log.write(msg)
+    log.close()
+    lock.release()
+
+    print("Cliente " + str(client_id) + " recibió los datos.")
 clientes = 1
 for i in range(clientes):
     t = threading.Thread(target=handle_client, args=((i+1,)))
--- ServerUDP.py
+++ ServerUDP.py
@@ -1,9 +1,15 @@
+import datetime
+import os
 import socketserver as SocketServer, threading, time
+
+path = ''
+lock = threading.lock()
 
 class ThreadedUDPRequestHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         data = self.request[0].decode("utf-8")
+        client_id = data
         socket = self.request[1]
         current_thread = threading.current_thread()
         print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
@@ -17,6 +23,20 @@ class ThreadedUDPRequestHandler(SocketServer.BaseRequestHandler):
         file.close()
         tiempo = fin - init
         print(tiempo)
+        
+        lock.acquire()
+        date = datetime.datetime.now()
+        name = str(date.year) + "-" + str(date.month) + "-" + str(date.day) + "-" + str(date.hour) + "-" + str(date.minute) + "-" + str(date.second)
+        file_stats = os.stat(path)
+        msg = "Nombre del archivo: " + path + " Tamanio del archivo: " + file_stats.st_size
+        msg += " Cliente: " + str(client_id)
+        msg += "\nEl tiempo calculado fue de: " + str(tiempo) +"\n"
+
+        log = open("Logs/"+name+"-log.txt","w")
+        log.write(msg)
+        log.close()
+        lock.release()
+
         ok =["R","1"] #socket.recieve(1024).decode("utf-8")
         msg = "Nombre del archivo: " + path + " Tamanio del archivo: " + tam
         msg += " Cliente: " + ok[1]
@@ -31,7 +51,7 @@ class ThreadedUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
     pass
 
 if __name__ == "__main__":
-    HOST, PORT = "0.0.0.0", 8888
+    HOST, PORT = "10.0.2.15", 8888
 
     server = ThreadedUDPServer((HOST, PORT), ThreadedUDPRequestHandler)
     f = int(input("Ingrese archivo que desea envíar. 1) Para 100mb 2) Para 250 mb."))
